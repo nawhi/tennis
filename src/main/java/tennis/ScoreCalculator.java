c7967975243c5f5599dd8131b6846913fa009229
Nick White <nawhi3@outlook.com>
2019-01-05 17:39:11 +0000

    Refactor to use map

--- src/main/java/tennis/ScoreCalculator.java
+++ src/main/java/tennis/ScoreCalculator.java
@@ -1,17 +1,24 @@
 package tennis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class ScoreCalculator {
     private Score score;
+    private static final Map<Integer, String> SCORES = new HashMap<>();
+
+    static {
+        SCORES.put(1, "Fifteen");
+        SCORES.put(2, "Thirty");
+    }
 
     ScoreCalculator(Score score) {
         this.score = score;
     }
 
     String score() {
-        if (score.player1() == 2)
-            return "Thirty-Love";
-        if (score.player1() == 1)
-            return "Fifteen-Love";
+        if (score.player1() > 0)
+            return SCORES.get(score.player1()) + "-Love";
         return "Love-All";
     }
 }
